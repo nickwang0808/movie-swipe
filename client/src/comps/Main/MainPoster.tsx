fa9b55cfe414e2d8c935ab3830e6edc30d3c9244
No-Tec <twernisch@gmail.com>
2020-10-21 20:54:43 -0400

    First commit to new branch

--- client/src/comps/Main/MainPoster.tsx
+++ client/src/comps/Main/MainPoster.tsx
@@ -6,7 +6,7 @@ interface IMainPosterProps {
 
 export default function MainPoster({ imgUrl }: IMainPosterProps) {
   const posterStyle = {
-    backgroundImage: `linear-gradient(25.4deg, rgba(255, 255, 255, 0) 51.03%, rgba(255, 255, 255, 0.3) 58.85%, rgba(255, 255, 255, 0.3) 99.3%), linear-gradient(360deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 20%), url(${imgUrl})`,
+    backgroundImage: `linear-gradient(25deg, rgba(255, 255, 255, 0) 51.03%, rgba(255, 255, 255, 0.3) 58.85%, rgba(255, 255, 255, 0.3) 99.3%), linear-gradient(360deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 20%), url(${imgUrl})`,
   };
 
   return (
