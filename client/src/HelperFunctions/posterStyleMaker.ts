76acd7811a98c073b9aead5005cd7d09ebcb4a69
No-Tec <twernisch@gmail.com>
2020-10-26 20:49:56 -0400

    Cleaning up Details screen.

--- client/src/HelperFunctions/posterStyleMaker.ts
+++ client/src/HelperFunctions/posterStyleMaker.ts
@@ -1,7 +1,6 @@
 const posterStyleMaker = (url: string) => {
   return {
-    backgroundImage: `url(${url})`,
-    // background: `linear-gradient(25deg, rgba(255, 255, 255, 0) 51.03%, rgba(255, 255, 255, 0.3) 58.85%, rgba(255, 255, 255, 0.3) 99.3%), linear-gradient(360deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 10%), url(${url})`,
+    backgroundImage: `linear-gradient(25deg, rgba(255, 255, 255, 0) 52%, rgba(255, 255, 255, 0.2) 53%, rgba(255, 255, 255, 0.2) 100%), url(${url})`,
   };
 };
 
