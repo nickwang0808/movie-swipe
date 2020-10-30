f8211de9f24fe16b04e3fce4af486ed57aab6fb3
No-Tec <twernisch@gmail.com>
2020-10-29 23:34:21 -0400

    now button have mobile friendly hitbox

--- client/src/comps/filter/Filters.tsx
+++ client/src/comps/filter/Filters.tsx
@@ -14,10 +14,9 @@ export default function Filters({
     <>
       <div className={style.container_filter}>
         <div className={sharedstyle.btn_close} onClick={() => setFilterOn(false)}>
-        <svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M9.98531 10.9853L2.00003 18.9706L0.58582 17.5563L8.57109 9.57108L1.00002 2L2.41423 0.585786L9.98531 8.15686L17.5564 0.585786L18.9706 2L11.3995 9.57108L19.3848 17.5563L17.9706 18.9706L9.98531 10.9853Z"/>
-</svg>
-
+          <svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.98531 10.9853L2.00003 18.9706L0.58582 17.5563L8.57109 9.57108L1.00002 2L2.41423 0.585786L9.98531 8.15686L17.5564 0.585786L18.9706 2L11.3995 9.57108L19.3848 17.5563L17.9706 18.9706L9.98531 10.9853Z"/>
+          </svg>
         </div>
         <div className={style.header}>
           <h1>Refine Suggestions</h1>
