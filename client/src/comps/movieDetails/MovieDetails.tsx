14066ecdb2ab0bd24a707aa3d385021b3c9a78b7
No-Tec <twernisch@gmail.com>
2020-10-29 22:17:15 -0400

    Fixing scrolling and movie details buttons

--- client/src/comps/movieDetails/MovieDetails.tsx
+++ client/src/comps/movieDetails/MovieDetails.tsx
@@ -55,23 +55,21 @@ export default function MovieDetails({
   );
 
   return (
-    <div className={style.details_content}>
-      <div className="background_container">
-        <div
-          className="background"
-          style={backgroundStyle(baseUrl + movieDetails?.poster_path)}
-        />
-      </div>
+    <>
+      <div
+        className="background"
+        style={backgroundStyle(baseUrl + movieDetails?.poster_path)} />
+    <div className={`${style.details_content} ${style.bottom_fade_out}`}>
+
       <div className={style.details_trailer}>
         {trailerUrl === undefined ? <div className="loader" /> : trailerDisplay}
-        {/* <div className="loader" />  */} {/* use this for testing  */}
+
       </div>
       <div className={style.container_moviedetails}>
         <div
           className={style.poster_1_inline}
           style={posterStyleMaker(baseUrl + movieDetails?.poster_path)}
-          onClick={() => setShowDetails(undefined)}
-        />
+          onClick={() => setShowDetails(undefined)} />
         <div className={style.details_title}>
           <h1>{movieDetails?.title}</h1>
         </div>
@@ -87,21 +85,19 @@ export default function MovieDetails({
               <path d="M14.5709 5.28981C14.4747 4.99399 14.2123 4.78455 13.9031 4.75658L9.68593 4.37371L8.01926 0.471701C7.89621 0.185024 7.61622 0 7.30457 0C6.99293 0 6.71283 0.185024 6.59056 0.471701L4.92389 4.37371L0.706 4.75658C0.396808 4.78511 0.134988 4.99455 0.0382398 5.28981C-0.0579506 5.58562 0.0308834 5.91008 0.264728 6.11517L3.45261 8.91049L2.51266 13.0504C2.44389 13.3548 2.56204 13.6695 2.81461 13.8521C2.95037 13.9507 3.10987 14 3.26993 14C3.40747 14 3.54512 13.9634 3.66806 13.8899L7.30457 11.7155L10.9404 13.8899C11.2071 14.0493 11.5425 14.0347 11.7945 13.8521C12.0471 13.6695 12.1653 13.3548 12.0965 13.0504L11.1565 8.91049L14.3444 6.11517C14.5782 5.91008 14.6671 5.58629 14.5709 5.28981Z" />
             </svg>
           </div>
-          {/* <img src="logo_imdb.png" height="16px" /> */}
+
         </div>
         <div className={style.details_tags}>
           <h3> {`${movieDetails && getGenres(movieDetails)}`}</h3>
           <h3 className={style.poster_overview_details}>
-            {`PG-13 | ${
-              movieDetails?.runtime
-            }min | ${movieDetails?.release_date.toString().slice(0, 4)}`}
+            {`PG-13 | ${movieDetails?.runtime}min | ${movieDetails?.release_date.toString().slice(0, 4)}`}
           </h3>
         </div>
       </div>
       <div className={style.container_info}>
         <div className={style.container_watch}></div>
         <div className={style.container_description}>
-          <div className={style.container_details_matched}>
+          {/* <div className={style.container_details_matched}>
             <div className={style.who_matched}>
               <div className={style.matched}>
                 <div className={style.forceSkew}>MATCH!</div>
@@ -119,39 +115,36 @@ export default function MovieDetails({
                   <path
                     fill-rule="evenodd"
                     clip-rule="evenodd"
-                    d="M0.5 13L19.5 13L19.5 15L0.5 15L0.5 13Z"
-                  />
+                    d="M0.5 13L19.5 13L19.5 15L0.5 15L0.5 13Z" />
                   <path
                     fill-rule="evenodd"
                     clip-rule="evenodd"
-                    d="M64.5 13L83.5 13L83.5 15L64.5 15L64.5 13Z"
-                  />
+                    d="M64.5 13L83.5 13L83.5 15L64.5 15L64.5 13Z" />
                 </svg>
               </div>
               <div>Partner</div>
             </div>
             <div className={`${sharedstyle.btn} ${sharedstyle.btn_outline}`}>
-              We've watched this!
+                        We've watched this!
             </div>
-          </div>
+          </div> */}
 
           <p>{movieDetails?.overview}</p>
         </div>
-        <div className={style.container_available}>
+        {/* <div className={style.container_available}>
           Available on
           <div className={style.available_icons}>
             <StreamingServiceButton />
           </div>
-        </div>
+        </div> */}
       </div>
-      {showVoting && (
+    </div>
+    {showVoting && (
         <VotingActions
           handleDislike={handleDislike}
           handleLike={handleLike}
           setShowDetails={() => setShowDetails(undefined)}
-          showDetail="Back"
-        />
-      )}
-    </div>
+          showDetail="Poster" />
+      )}</>
   );
 }
