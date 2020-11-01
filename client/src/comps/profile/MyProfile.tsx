18a53e16c19d9674d048ce81e9a379e33e42ee86
Nick Wang <wang703003515@gmail.com>
2020-11-01 15:23:07 +0800

    added delete account

--- client/src/comps/profile/MyProfile.tsx
+++ client/src/comps/profile/MyProfile.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ListViewButton from "../ButtonComps/ListViewButton";
 // import { auth } from "../../firebase/config";
 import style from "./MyProfile.module.css";
-import Modal from "../notification/modal"
+import Modal from "../notification/modal";
 import { cfaSignOut } from "capacitor-firebase-auth";
+import { auth } from "../../firebase/config";
 
 export default function MyProfile() {
   return (
@@ -11,16 +12,20 @@ export default function MyProfile() {
       {/* <Modal /> */}
       <h1>My Profile</h1>
       <div className={style.settings_container}>
-        <ListViewButton
-          name="Watch Groups" 
-        />
+        <ListViewButton name="Watch Groups" />
         <ListViewButton name="Disliked Media" />
         <ListViewButton name="About MediaSync" />
         <ListViewButton
           name="Sign Out"
           action={() => cfaSignOut().subscribe()}
         />
-        <ListViewButton name="Delete Account" />
+        <ListViewButton
+          name="Delete Account"
+          action={() => {
+            console.log("deleted");
+            auth.currentUser?.delete().then(() => window.location.reload());
+          }}
+        />
       </div>
     </div>
   );
