549db8a57f9846b09d01ccbcc381b225a4f13cfd
Nick Wang <wang703003515@gmail.com>
2020-11-02 14:40:55 +0800

    router refactoed

--- client/src/comps/Main/VotingActions.tsx
+++ client/src/comps/Main/VotingActions.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import sharedstyle from "../ButtonComps/ButtonComps.module.css";
 import DownVote from "../ButtonComps/DownVote";
 import UpVote from "../ButtonComps/UpVote";
@@ -6,25 +7,26 @@ import UpVote from "../ButtonComps/UpVote";
 interface IVotingActionsProps {
   handleDislike: () => void;
   handleLike: () => void;
-  setShowDetails: () => void;
   showDetail: string;
+  goTo: string;
 }
 
 export default function VotingActions({
   handleDislike,
   handleLike,
-  setShowDetails,
   showDetail,
+  goTo,
 }: IVotingActionsProps) {
   return (
     <div className="container_vote">
       <DownVote handleDislike={handleDislike} />
-      <div className={sharedstyle.container_detailsbtn} onClick={setShowDetails}>
-        <div
+      <div className={sharedstyle.container_detailsbtn}>
+        <Link
+          to={goTo}
           className={`${sharedstyle.btn} ${sharedstyle.btn_details}`}
         >
           {showDetail}
-        </div>
+        </Link>
       </div>
       <UpVote handleLike={handleLike} />
     </div>
--- client/src/comps/profile/MyProfile.tsx
+++ client/src/comps/profile/MyProfile.tsx
@@ -5,32 +5,36 @@ import Modal from "../notification/modal";
 import { cfaSignOut } from "capacitor-firebase-auth";
 import { auth } from "../../firebase/config";
 import WatchGroups from "./WatchGroups";
-import { Link, Route } from "react-router-dom";
+
 import { IonRouterOutlet } from "@ionic/react";
+import { Route } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function MyProfile() {
   return (
     <div>
       {/* <Modal /> */}
-      <h1>My Profile</h1>
-      <div className={style.settings_container}>
-        <Link to="/profile/watch-groups">
-          <ListViewButton name="Watch Groups" />
-        </Link>
-        <ListViewButton name="Disliked Media" />
-        <ListViewButton name="About MediaSync" />
-        <ListViewButton
-          name="Sign Out"
-          action={() => cfaSignOut().subscribe()}
-        />
-        <ListViewButton
-          name="Delete Account"
-          action={() => {
-            console.log("deleted");
-            auth.currentUser?.delete().then(() => window.location.reload());
-          }}
-        />
-      </div>
+      <Route exact path="/profile">
+        <h1>My Profile</h1>
+        <div className={style.settings_container}>
+          <Link className="link" to="/profile/watch-groups">
+            <ListViewButton name="Watch Groups" />
+          </Link>
+          <ListViewButton name="Disliked Media" />
+          <ListViewButton name="About MediaSync" />
+          <ListViewButton
+            name="Sign Out"
+            action={() => cfaSignOut().subscribe()}
+          />
+          <ListViewButton
+            name="Delete Account"
+            action={() => {
+              console.log("deleted");
+              auth.currentUser?.delete().then(() => window.location.reload());
+            }}
+          />
+        </div>
+      </Route>
       <Route exact path="/profile/watch-groups">
         <WatchGroups />
       </Route>
