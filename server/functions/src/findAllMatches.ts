9330444027247df43ed2ca92f1209d727fffc894
Nick Wang <wang703003515@gmail.com>
2020-11-09 07:16:08 +0000

    sever side sync

--- server/functions/src/findAllMatches.ts
+++ server/functions/src/findAllMatches.ts
@@ -2,9 +2,15 @@ import * as functions from "firebase-functions";
 import { db } from ".";
 import arrayChunks from "./HelperFunctions/ArrayChunks";
 
+interface IUserInfo {
+  email: string;
+  name: string | null;
+  uid: string;
+}
+
 interface result {
   matchedMovie: number;
-  friendUid: string[];
+  friendInfo: IUserInfo[];
 }
 
 const findAllMatches = functions.https.onCall(async (data, context) => {
@@ -15,40 +21,49 @@ const findAllMatches = functions.https.onCall(async (data, context) => {
     const matchedFriendsLikes: result[] = [];
     await Promise.all(
       myFriends.map(async (chunks) => {
-        console.log("chunks", chunks);
         const query = db
           .collectionGroup("User_Details")
           .where("uid", "in", chunks);
         const querySnapShot = await query.get();
-        console.log("querySnapShot", querySnapShot.docs);
-        querySnapShot.docs.map((doc) => {
-          const data = doc.data();
-          console.log("data", data);
-          const friendsLike: number[] = data.liked_movies;
-          const matchedMovies = myLikes.filter((element) =>
-            friendsLike.includes(element)
-          );
-          if (matchedMovies) {
-            const friendUid: string = data.uid;
-            matchedMovies.forEach((movie) => {
-              const foundIndex = matchedFriendsLikes.findIndex(
-                (elem) => elem.matchedMovie === movie
-              );
-              if (foundIndex === -1) {
-                matchedFriendsLikes.push({
-                  matchedMovie: movie,
-                  friendUid: [friendUid],
-                });
-              } else {
-                matchedFriendsLikes[foundIndex].friendUid.push(friendUid);
-              }
-              return;
-            });
-          }
-          return;
-        });
+        await Promise.all(
+          querySnapShot.docs.map(async (doc) => {
+            const data = doc.data();
+            const friendsLike: number[] = data.liked_movies;
+            const matchedMovies = myLikes.filter((element) =>
+              friendsLike.includes(element)
+            );
+            if (matchedMovies) {
+              const friendUid: string = data.uid;
+              const userInfoDoc = await db
+                .collection("Users")
+                .doc(friendUid)
+                .get();
+              const userDetails = userInfoDoc.data();
+
+              matchedMovies.forEach((movie) => {
+                const foundIndex = matchedFriendsLikes.findIndex(
+                  (elem) => elem.matchedMovie === movie
+                );
+                if (foundIndex === -1) {
+                  matchedFriendsLikes.push({
+                    matchedMovie: movie,
+                    friendInfo: [userDetails as IUserInfo],
+                  });
+                } else {
+                  matchedFriendsLikes[foundIndex].friendInfo.push(
+                    userDetails as IUserInfo
+                  );
+                }
+                return;
+              });
+            }
+            return;
+          })
+        );
       })
     );
+
+    console.log("matchedFriendsLikes", matchedFriendsLikes);
     return matchedFriendsLikes;
   } else {
     throw new functions.https.HttpsError(
