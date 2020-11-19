832473daf1c0d7a21aa5d4705238049ca37848c3
No-Tec <twernisch@gmail.com>
2020-11-18 22:21:59 -0500

    WatchedListUpdates

--- client/src/comps/auth/SignInScreen.tsx
+++ client/src/comps/auth/SignInScreen.tsx
@@ -139,14 +139,14 @@ export default function SignInScreen() {
         >
           {isSignUp ? "Log In" : "Sign Up"}
         </button>
-        <button
+        {/* <button
           onClick={(e) => {
             e.preventDefault();
             history.goBack();
           }}
         >
           back
-        </button>
+        </button> */}
       </form>
     </>
   );
@@ -154,7 +154,20 @@ export default function SignInScreen() {
   return (
     <div className="container_allcontent">
       <h1>
-        <BackButton linkTo="history.goBack()" />
+      <div className={sharedstyle.btn_Back} onClick={(e) => {
+            e.preventDefault();
+            history.goBack();
+          }}>
+      <svg
+        width="20"
+        height="18"
+        viewBox="0 0 20 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M10.7071 1.70712L9.29292 0.292908L0.585815 9.00001L9.29292 17.7071L10.7071 16.2929L4.41423 10H19.5V8H4.41426L10.7071 1.70712Z" />
+      </svg>
+    </div>
         Register / Log In
       </h1>
       <div className={style.login_container}>
