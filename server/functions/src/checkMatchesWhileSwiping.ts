3550fabc4be303446022157677a6686eee8e3521
Nick Wang <wang703003515@gmail.com>
2020-12-14 12:54:12 +0000

    swiping match optimization

--- server/functions/src/checkMatchesWhileSwiping.ts
+++ server/functions/src/checkMatchesWhileSwiping.ts
@@ -1,11 +1,5 @@
 import * as functions from "firebase-functions";
-import {
-  arrayUnion,
-  collectionName,
-  db,
-  IProfileDetails,
-  IVotedMovies,
-} from ".";
+import { arrayUnion, collectionName, db, IVotedMovies } from ".";
 
 export const checkMatchesWhileSwiping = functions.firestore
   .document(
@@ -15,13 +9,19 @@ export const checkMatchesWhileSwiping = functions.firestore
     const myUid: string = context.params.myUid;
     const likedMovie: string = context.params.likedMovie;
 
-    db.runTransaction(async (t) => {
+    await db.runTransaction(async (t) => {
       const allFriends = db
         .collection(collectionName.User)
         .doc(myUid)
         .collection(collectionName.Friends);
       // get all friends first
       const allFriendsDocs = await t.get(allFriends);
+
+      console.log("checkMatchesWhileSwiping");
+
+      if (allFriendsDocs.empty) return;
+      console.log("friends found");
+
       const friendsIds: string[] = [];
       allFriendsDocs.forEach((doc) => {
         const friendId = doc.id;
@@ -42,37 +42,81 @@ export const checkMatchesWhileSwiping = functions.firestore
       });
 
       // my profile
-      const myProfile = (
-        await db.collection(collectionName.User).doc(myUid).get()
-      ).data() as IProfileDetails;
+      const myProfile = extractProfile(snap.data() as IVotedMovies);
 
       const myMatches = matchedVotedMovies.map((elem) => extractProfile(elem));
 
       await Promise.all(
         matchedVotedMovies.map(async (votedMovie) => {
-          // set my like
-          await db
-            .collection(collectionName.User)
-            .doc(myUid)
-            .collection(collectionName.Liked)
-            .doc(String(votedMovie.id))
-            .update({
-              matchedWith: arrayUnion(...myMatches),
-            });
+          const batch = db.batch();
 
-          // set notification and swap my info in
-          await db
-            .collection(collectionName.User)
-            .doc(votedMovie.uid) // friend's uid
-            .collection(collectionName.Liked)
-            .doc(String(votedMovie.id))
-            .set({
-              matchedWith: arrayUnion(myProfile),
-            });
+          // set friends notification and swap my info in
+          batch.update(
+            db
+              .collection(collectionName.User)
+              .doc(votedMovie.uid) // friend's uid
+              .collection(collectionName.Liked)
+              .doc(String(votedMovie.id)),
+            { matchedWith: arrayUnion(myProfile) }
+          );
+
+          // now set friends notification
+          batch.set(
+            db
+              .collection(collectionName.User)
+              .doc(votedMovie.uid) // friend's uid
+              .collection(collectionName.Notifications)
+              .doc(String(votedMovie.id)),
+            {
+              ...removeProfile(votedMovie),
+              matchedWith: [myProfile],
+            }
+          );
+
+          // set my like
+          batch.update(
+            db
+              .collection(collectionName.User)
+              .doc(myUid)
+              .collection(collectionName.Liked)
+              .doc(String(votedMovie.id)),
+            {
+              matchedWith: arrayUnion(...myMatches),
+            }
+          );
+
+          // set my notification
+          batch.set(
+            db
+              .collection(collectionName.User)
+              .doc(myUid)
+              .collection(collectionName.Notifications)
+              .doc(String(votedMovie.id)),
+            {
+              ...removeProfile(votedMovie),
+              matchedWith: myMatches,
+            }
+          );
+
+          batch.commit();
+          return;
         })
       );
+      return;
     });
+    return;
   });
+
+function removeProfile(input: any) {
+  delete input.displayName;
+  delete input.email;
+  delete input.isAnonymous;
+  delete input.photoURL;
+  delete input.uid;
+  delete input.genrePreference;
+
+  return input;
+}
 
 function extractProfile(data: IVotedMovies) {
   const {
