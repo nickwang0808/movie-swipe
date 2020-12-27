ed6c7611e9784acd14ca7835418738f9ae9473ea
Nick Wang <wang703003515@gmail.com>
2020-12-27 16:49:53 +0800

    renamed everything to client

--- client/ios/App/public/static/js/53.ee43e17a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient2=this.webpackJsonpclient2||[]).push([[53],{171:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(12),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.j)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.ee43e17a.chunk.js.map
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,23 +1,28 @@
-import { IonApp, IonRouterOutlet } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Provider } from "react-redux";
+import AuthChecker from "./Screens/Auth/AuthChecker";
 import * as serviceWorker from "./serviceWorker";
-import Store from "./store";
+import { store } from "./store";
 
-ReactDOM.render(
-  <Store>
-    <IonApp>
-      <IonReactRouter>
-        <IonRouterOutlet>
-          <App />
-        </IonRouterOutlet>
-      </IonReactRouter>
-    </IonApp>
-  </Store>,
-  document.getElementById("root")
-);
+const render = () => {
+  const App = require("./App").default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <AuthChecker>
+        <App />
+      </AuthChecker>
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept("./App", render);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
