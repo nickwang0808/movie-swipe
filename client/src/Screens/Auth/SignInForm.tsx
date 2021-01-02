fbd84dfce586d56118c634beea6512006fef4108
Nick Wang <wang703003515@gmail.com>
2021-01-02 11:00:52 +0800

    added sign in for meail

--- client/src/Screens/Auth/SignInForm.tsx
+++ client/src/Screens/Auth/SignInForm.tsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components/macro";
-import { Btn } from "../../theme/BaseComp";
+import { auth } from "../../firebase/config";
+import newUserDBInit from "../../Helper/newUserDBInit";
+import { Btn, ErrorMessage } from "../../theme/BaseComp";
 
 interface iProps {
   handleSignInEmail: (email: string, password: string) => void;
 }
 
 export default function SignInForm({ handleSignInEmail }: iProps) {
+  const [isSignUp, setIsSignUp] = useState(true);
+  const [signInErr, setSignInErr] = useState<string | null>(null);
+
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = ({ email, password }: { email: string; password: string }) =>
-    handleSignInEmail(email, password);
+  const onSubmit = ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    if (isSignUp) {
+      handleSignInEmail(email, password);
+      return;
+    } else {
+      auth
+        .signInWithEmailAndPassword(email, password)
+        .then((result) => {
+          newUserDBInit({
+            displayName: email,
+            email,
+            isAnonymous: false,
+            photoURL: "",
+            uid: result.user?.uid as string,
+          });
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          const errorMessage = error.message;
+          // console.log(errorMessage);
+          setSignInErr(errorMessage);
+          // setError(errorMessage);
+        });
+    }
+  };
   console.log(errors);
 
   return (
@@ -29,7 +63,16 @@ export default function SignInForm({ handleSignInEmail }: iProps) {
           ref={register({ required: true })}
         />
 
-        <StyledSubmit as="input" type="submit" value="Sign up with Email" />
+        <StyledSubmit
+          as="input"
+          type="submit"
+          value={isSignUp ? "Sign up with Email" : "Login"}
+        />
+        {signInErr && <StyledErr>{signInErr}</StyledErr>}
+
+        <StyledLinkish onClick={() => setIsSignUp((prev) => !prev)}>
+          {isSignUp ? "Login Instead" : "Don't have an account? Sign Up Here"}
+        </StyledLinkish>
       </form>
     </FormWrapper>
   );
@@ -73,4 +116,18 @@ const StyledSubmit = styled(Btn)`
     border-color: var(--highlight);
     color: var(--highlight);
   }
+`;
+
+const StyledLinkish = styled.div`
+  margin: 0 auto;
+  text-decoration: underline;
+  color: var(--dark);
+  font-size: 1.8rem;
+  line-height: 7rem;
+`;
+
+const StyledErr = styled(ErrorMessage).attrs({
+  className: "ion-text-center",
+})`
+  margin: 1rem auto 0 auto;
 `;
